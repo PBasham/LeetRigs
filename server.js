b48a67f672d0e2d5317d77b1b48f28b6e31bcf94
patrickbasham <PatrickTBasham@gmail.com>
2022-07-01 15:48:39 -0400

    added comments to server.js file, middleware, port and listener. Updated layout for app.jsx to empty page

--- server.js
+++ server.js
@@ -8,3 +8,35 @@ const logger = require("morgan")
 
 require("dotenv").config()
 require("./config/database")
+
+/*========================================
+        Middleware
+========================================*/
+const app = express()
+
+app.use(logger("dev"))
+app.use(express.json())
+
+app.use(favicon(path.join(__dirname, "build", "favicon.ico")))
+app.use(express.static(path.join(__dirname, "build")))
+
+// checkToken will go here
+
+/*========================================
+        API routes will go here
+========================================*/
+/// user apu first 
+
+// ensure logged in middleware will go here.
+// all othe APIs
+
+// catch all route that will return the index.html on all non-AJAX request.
+
+/*========================================
+        Port and Listener
+========================================*/
+const port = process.env.PORT || 3001
+
+app.listen(port, () => {
+    console.log(`Expesss app running on port ${port}`)
+})
