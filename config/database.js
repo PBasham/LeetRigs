4560e6073ed31795ffe3c011d3b246df96e53379
patrickbasham <PatrickTBasham@gmail.com>
2022-07-05 10:25:39 -0400

    Added connection to database in database.js and fetch in seed.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,43 @@
+/*========================================
+        Import Dependencies
+========================================*/
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL);
-
+/*========================================
+        Database Connection
+========================================*/
 const db = mongoose.connection;
 
-db.on('connected', function () {
-  console.log(`Connected to ${db.name} at ${db.host}: ${db.port}`);
-});
+/*========================================
+        This will be used when we use Atlas for the database
+========================================*/
+// Connetion to Atlas
+// const MONGODB_URI = process.env.MONGODB_URI
+// const CONFIG = {
+//   useNewUrlParser: true,
+//   useInifiedTopology: true,
+// }
 
+// mongoose.connect(MONGODB_URI, CONFIG) // Establish connection to database
+
+//Connection to loval MongoDB
+const DATABASE_URL  = process.env.DATABASE_URL
+const CONFIG = {
+    useNewUrlParser: true,
+    useInifiedTopology: true,
+}
+
+mongoose.connect(DATABASE_URL, CONFIG)
+
+db.on('connected', () => {
+  console.log(`Connected to ${db.name} at ${db.host}: ${db.port}`);
+})
+.on("close", () => {
+  console.log(`Disconnected from ${db.name} at ${db.host}: ${db.port}`)
+})
+.on("error", (error) => console.log(error))
+
+/*========================================
+        Export Modules
+========================================*/
+module.exports = mongoose
