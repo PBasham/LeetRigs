a35736f68b455cc0a4a9c4ba5a45b5e0590dbd72
IanDLeon <ian.reggie.deleon@gmail.com>
2022-07-05 13:42:35 -0400

    Authentication Page

---
+++ src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+import LoginForm from '../../components/LoginForm/LoginForm';
+import SignUpFrom from '../../components/SignUpForm/SignUpForm';
+
+export default function AuthPage({ setUser }) {
+  const [showLogin, setShowLogin] = useState(true);
+
+  return (
+    <main className="AuthPage">
+    <div>
+      <h1 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG In'}</h1>
+    </div>
+    {showLogin ? <LoginForm setUser={setUser}/> : <SignUpFrom setUser={setUser}/>}
+    </main>
+  )
+}
