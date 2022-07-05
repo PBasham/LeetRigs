4964f647cdcdad1220d1d6c96d6108b55fea5402
Robert Romano <bromano1992@gmail.com>
2022-07-05 11:26:57 -0400

    Added checktoken functionality in config and exported to users route

---
+++ config/checkToken.js
@@ -0,0 +1,27 @@
+/*========================================
+        DEPENDENCIES
+========================================*/
+const jwt = require('jsonwebtoken');
+
+/*========================================
+        CHECKTOKEN FUNCTION
+========================================*/
+function checkToken(req, res, next) {
+  let token = req.get('Authorization') || req.query.token;
+  if (token) {
+    token = token.replace('Bearer ', '');
+    jwt.verify(token, process.env.SECRET, function (err, decoded) {
+      req.user = err ? null : decoded.user;
+      req.exp = err ? null : new Date(decoded.exp * 1000)
+    })
+    return next();
+  } else {
+    req.user = null;
+    return next();
+  }
+}
+
+/*========================================
+        EXPORTS
+========================================*/
+module.exports = checkToken
