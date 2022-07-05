791fe73f718fd79b28b7a8311c22b54518e9f606
Robert Romano <bromano1992@gmail.com>
2022-07-05 13:16:00 -0400

    Removed extra space

--- seed.js
+++ seed.js
@@ -4,6 +4,7 @@
 require("dotenv").config()
 const { element } = require("prop-types")
 const mongoose = require("./config/database.js")
+// const fetch = require("node-fetch")
 // require("./config/database.js")
 
 const Item = require("./models/item.js")
@@ -12,8 +13,7 @@ const db = mongoose.connection
 db.on("open", () => {
     Item.deleteMany({})
     .then((deletedItems) => {
-
-        fetch('https://fakestoreapi.com/products')
+      fetch('https://fakestoreapi.com/products')
         .then((items) => {
             items.json()
             .then((itemsJson) => {
