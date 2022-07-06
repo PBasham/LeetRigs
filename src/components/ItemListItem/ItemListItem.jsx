08e3210967eb670bfc6e92ed5cadbf3f19b991a6
Robert Romano <bromano1992@gmail.com>
2022-07-06 13:09:47 -0400

    starting css for itemlistitems

--- src/components/ItemListItem/ItemListItem.jsx
+++ src/components/ItemListItem/ItemListItem.jsx
@@ -1,7 +1,13 @@
+import "./ItemListItem.css"
+
 export default function ItemListItem({ storeItem }) {
     return (
-        <div className="item-card">
+        <div className="item-container">
+            <div className="item-card">
             <h2>{storeItem.title}</h2>
+            <img src={storeItem.image} width="200" alt="itempic" />
+                <h4>${storeItem.price}</h4>
+                </div>
         </div>
     )
 }
