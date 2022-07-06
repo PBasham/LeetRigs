9cc323043233afc655ba09afd2ecd322dab868f0
patrickbasham <PatrickTBasham@gmail.com>
2022-07-06 15:44:06 -0400

    added onClick to itemDetal and ItemListItem

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -3,9 +3,14 @@
 ========================================*/
 import "./ItemDetail.css"
 
-export default function ItemDetail(props) {
+export default function ItemDetail({ setShowItemDetail }) {
+
+    const leaveDetailWindow = () => {
+        setShowItemDetail(false)
+    }
+
     return (
-        <div className="item-detail-container">
+        <div className="item-detail-container" onClick={leaveDetailWindow}>
             <div className="item-detail-card">
                 item-Detail
             </div>
