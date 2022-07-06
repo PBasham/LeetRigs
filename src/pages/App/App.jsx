bfb04e9b2877e7df156223ad850fb027c07fabd9
Robert Romano <bromano1992@gmail.com>
2022-07-06 11:08:46 -0400

    Made a few changes on app page

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -34,7 +34,7 @@ function App() {
                                   
                           </>
                           :
-                          <AuthPage />}
+                          <AuthPage setUser={setUser} />}
     </div>
   );
 }
