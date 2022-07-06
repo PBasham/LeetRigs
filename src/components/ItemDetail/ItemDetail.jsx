c6994bc65184a2bcb0ee798dbe8515d765e5282a
patrickbasham <PatrickTBasham@gmail.com>
2022-07-06 16:39:38 -0400

    added styling and details to ItemDetail page

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -5,13 +5,14 @@ import "./ItemDetail.css"
 
 export default function ItemDetail({ setShowItemDetail }) {
 
-    const leaveDetailWindow = () => {
-        setShowItemDetail(false)
+    const leaveDetailWindow = (e) => {
+        setShowItemDetail(false);
     }
 
     return (
-        <div className="item-detail-container" onClick={leaveDetailWindow}>
+        <div className="item-detail-container">
             <div className="item-detail-card">
+                <button className="item-detail-close-btn" onClick={leaveDetailWindow}>X</button>
                 item-Detail
             </div>
         </div>
