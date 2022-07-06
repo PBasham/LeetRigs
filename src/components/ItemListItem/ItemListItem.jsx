64c6a637570af8f7840beb21bc4b086c3ba705b3
patrickbasham <PatrickTBasham@gmail.com>
2022-07-06 13:45:02 -0400

    styled ItemList and ItemPage

--- src/components/ItemListItem/ItemListItem.jsx
+++ src/components/ItemListItem/ItemListItem.jsx
@@ -2,12 +2,10 @@ import "./ItemListItem.css"
 
 export default function ItemListItem({ storeItem }) {
     return (
-        <div className="item-container">
             <div className="item-card">
-            <h2>{storeItem.title}</h2>
-            <img src={storeItem.image} width="200" alt="itempic" />
-                <h4>${storeItem.price}</h4>
-                </div>
-        </div>
+                <h3 className="item-title">{storeItem.title}</h3>
+                <img  className="item-img"src={storeItem.image} alt="itempic" />
+                <p className="item-price">${storeItem.price}</p>
+            </div>
     )
 }
