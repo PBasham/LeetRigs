c90c3250aade251b06f8f83817c70669ad60e874
IanDLeon <ian.reggie.deleon@gmail.com>
2022-07-06 15:55:43 -0400

    added LogOut to navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,19 +8,21 @@ import { Link } from "react-router-dom"
 ========================================*/
 import CheckoutPage from "../../pages/CheckoutPage/CheckoutPage"
 import ItemPage from "../../pages/ItemPage/ItemPage"
+import UserLogOut from "../UserLogOut/UserLogOut"
 /*========================================
         import Css
 ========================================*/
 import "../../index.css"
 import "./NavBar.css"
 
-export default function NavBar(props) {
+export default function NavBar({ user, setUser }) {
     return (
         <div className="nav-bar">
             <Link  to="/Home" element={<ItemPage />}><button className="nav-btn flex-start">Home</button></Link>
             <button className="nav-btn flex-start">Order History</button>
 
             <div className="flex-end">
+                <UserLogOut user={user} setUser={setUser}/>
                 <button className="nav-btn">LogOut</button>
                 <Link  to="/checkout" element={<CheckoutPage />}><button className="nav-btn cart">Cart</button></Link>
             </div>
