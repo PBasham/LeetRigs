b6645be3bfca4614d89772a4ef51cb52548a2e7c
patrickbasham <PatrickTBasham@gmail.com>
2022-07-07 02:27:35 -0400

    updated styling for navBar and made so buttons will mark as active and no longer be clickable when on respective page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 /*========================================
         Import dependencies
 ========================================*/
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 /*========================================
@@ -16,15 +17,60 @@ import "../../index.css"
 import "./NavBar.css"
 
 export default function NavBar({ user, setUser }) {
+    // create useStates
+    const [homeActive, setHomeActive] = useState(true)
+    const [orderHistoryActive, setOrderHistoryActive] = useState(false)
+    const [checkoutPageActive, setCheckoutPageActive] = useState(false)
+
+    /*========================================
+            functions
+    ========================================*/
+    const homeClicked = () => {
+        allInactive()
+        setHomeActive(!homeActive)
+    }
+    const orderHistoryClicked = () => {
+        allInactive()
+        setOrderHistoryActive(!orderHistoryActive)
+    }
+    const checkoutPageClicked = () => {
+        allInactive()
+        setCheckoutPageActive(!checkoutPageActive)
+    }
+
+    const allInactive = () => {
+        setHomeActive(false)
+        setOrderHistoryActive(false)
+        setCheckoutPageActive(false)
+    }
+
     return (
         <div className="nav-bar">
-            <Link  to="/Home" element={<ItemPage />}><button className="nav-btn flex-start">Home</button></Link>
+            { !homeActive ?
+            <Link
+                to="/Home"
+                element={<ItemPage />}
+                onClick={homeClicked}
+                >
+                <button
+                    className="nav-btn flex-start">
+                    Home
+                </button>
+            </Link>
+            :
+                <button
+                    className="nav-btn flex-start nav-btn-active">
+                    Home
+                </button>
+            }
+
+
             <button className="nav-btn flex-start">Order History</button>
 
             <div className="flex-end">
-                <UserLogOut user={user} setUser={setUser}/>
+                <UserLogOut user={user} setUser={setUser} />
                 <button className="nav-btn">LogOut</button>
-                <Link  to="/checkout" element={<CheckoutPage />}><button className="nav-btn cart">Cart</button></Link>
+                <Link to="/checkout" element={<CheckoutPage />}><button className="nav-btn cart">Cart</button></Link>
             </div>
         </div>
     )
