0be6067d8d9a5d5462d25e098e27631202ff7470
Robert Romano <bromano1992@gmail.com>
2022-07-07 03:14:55 -0400

    Created checkoutarea component, get route for orders functional

---
+++ src/components/CheckoutArea/CheckoutArea.jsx
@@ -0,0 +1,8 @@
+export default function CheckoutArea({ cart, setCart }) {
+  return (
+    <div className="checkout">
+      <h1>test</h1>
+      {console.log(cart)}
+    </div>
+  )
+}
--- src/pages/CheckoutPage/CheckoutPage.jsx
+++ src/pages/CheckoutPage/CheckoutPage.jsx
@@ -1,9 +1,26 @@
-export default function CheckoutPage(props) {
+import { useState, useEffect } from 'react'
+import CheckoutArea from "../../components/CheckoutArea/CheckoutArea";
+import * as OrderApi from "../../utilities/orders-api.js"
+
+
+export default function CheckoutPage() {
+
+    const [cart, setCart] = useState([])
+    
+    useEffect(function() {
+        (async function getUserCart(){
+            const userCart = await OrderApi.findCart()
+            setCart(userCart)
+        })()
+    }, [])
+
+
 return (
     <div className="checkout-page-div">
         <h1>CheckOut</h1>
         <div className="checkout-order">
             CheckOut Items
+            <CheckoutArea cart={cart} setCart={setCart}/>
         </div>
 
     </div>
