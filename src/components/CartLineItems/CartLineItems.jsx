df62d1a31d94f611325a1718467983840d26dca0
patrickbasham <PatrickTBasham@gmail.com>
2022-07-07 20:19:09 -0400

    added comment for CartLineItems

--- src/components/CartLineItems/CartLineItems.jsx
+++ src/components/CartLineItems/CartLineItems.jsx
@@ -6,6 +6,7 @@ export default function CartLineItems({ lineItem, key}) {
     <h6>{ lineItem.item.title }</h6>
     <img src={lineItem.item.image} alt="" />
     <p className="checkout-item-price">qty:{ lineItem.qty }</p> <p>${lineItem.item.price}</p>
+    {/* Two buttons, one for subtract one and one for add 1 */}
   </div>
 )
 }
