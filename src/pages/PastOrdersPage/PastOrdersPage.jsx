ffccb21fbb0bbb39ae895f71240394a7b4ccbb99
Robert Romano <bromano1992@gmail.com>
2022-07-09 01:32:39 -0400

    Setting up rendering for orders

--- src/pages/PastOrdersPage/PastOrdersPage.jsx
+++ src/pages/PastOrdersPage/PastOrdersPage.jsx
@@ -1,8 +1,28 @@
+import * as OrderApi from '../../utilities/orders-api'
+import { useEffect, useState } from 'react'
+
+
 export default function PastOrdersPage() {
 
+  const [orders, setOrders] = useState(null)
 
-  return (
-    <div>MY PAST ORDERS</div>
+  let userOrders = OrderApi.pastOrders()
+
+  useEffect(function() {
+    (async function getUserOrders(){
+        setOrders(userOrders)
+    })()
+}, [])
+
+
+
+{
+  userOrders.map(myOrder => { myOrder }
+  )
+}
+return (
+  <div>MY PAST ORDERS</div>
+
   )
 }
 
