4968bf88c5ddce602a70760f364ea854ec9ff434
patrickbasham <PatrickTBasham@gmail.com>
2022-07-09 01:57:10 -0400

    added styling for item detail card

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -33,14 +33,9 @@ export default function ItemDetail({ setShowItemDetail, itemDetail, cart, setCar
                 <button className="item-detail-close-btn" onClick={leaveDetailWindow}>X</button>
                 <h3 className="item-detail-name">{itemDetail.title}</h3>
                     <img className="item-detail-img" src={itemDetail.image}></img>
-                <p className="item-detail-desc">{itemDetail.description}</p>
                 <div className="info">
-                    <div className="flex-start">
                         <p className="item-detail-price">Price: ${itemDetail.price}</p>
-                    </div>
-                    <div className="flex-end">
                         <button className="btn-add" onClick={handleClick}>Add To Cart</button>
-                    </div>
                 </div>
             </div>
         </div>
