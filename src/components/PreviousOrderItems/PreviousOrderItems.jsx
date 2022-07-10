7a5f88b8f385e7507372cb5f35207cda028050c5
Robert Romano <bromano1992@gmail.com>
2022-07-10 02:17:25 -0400

    Previous order items now rendering

--- src/components/PreviousOrderItems/PreviousOrderItems.jsx
+++ src/components/PreviousOrderItems/PreviousOrderItems.jsx
@@ -3,8 +3,17 @@ import React from 'react'
 export default function PreviousOrderItems({ orderItem }) {
 
   return (
-    <div>PreviousOrderItems
-      {orderItem._id}
+    <>
+      Ordered on: {orderItem.item.createdAt}
+    <div className='order-item-name'>
+      {orderItem.item.title}
     </div>
+      <div className='order-item-price'>
+        ${orderItem.item.price}
+      </div>
+      <div>
+        <img className='order-item-pic' src={orderItem.item.image} alt="itempic" />
+      </div>
+      </>
   )
 }
