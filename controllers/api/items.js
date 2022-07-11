7238c2bf579d8c5122ffabb6e0d8a949aded37c3
Robert Romano <bromano1992@gmail.com>
2022-07-11 00:14:59 -0400

    Added route and controller function for item search

--- controllers/api/items.js
+++ controllers/api/items.js
@@ -1,26 +1,32 @@
 /*========================================
         DEPENDENCIES
 ========================================*/
-const Item = require('../../models/item')
+const Item = require('../../models/item');
 
 /*========================================
         ITEM FUNCTIONS
 ========================================*/
 async function index(req, res) {
-  const items = await Item.find({})
-  //   .sort('name')
-  //   .populate('category')
-  //   .exec();
-  // items.sort((a, b) => a.category.sortOrder - b.category.sortOrder);
-  res.json(items);
+	const items = await Item.find({});
+	//   .sort('name')
+	//   .populate('category')
+	//   .exec();
+	// items.sort((a, b) => a.category.sortOrder - b.category.sortOrder);
+	res.json(items);
 }
 
 async function show(req, res) {
-  const item = await Item.findById(req.params.id);
-  res.json(item);
+	const item = await Item.findById(req.params.id);
+	res.json(item);
+}
+
+async function itemSearch(req, res) {
+        const itemquery = await Item.findOne(req.params.title);
+        res.json(itemquery)
+                
 }
 
 /*========================================
         EXPORTS
 ========================================*/
-module.exports = { index, show }
+module.exports = { index, show, itemSearch };
