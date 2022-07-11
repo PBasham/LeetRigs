896f4abf2717dfece6258b18883b3db7a4e76e24
patrickbasham <PatrickTBasham@gmail.com>
2022-07-10 23:00:07 -0400

    fixed issue with fixed position navbar and updated styling / functionality for itemcard div

--- src/components/ItemListItem/ItemListItem.jsx
+++ src/components/ItemListItem/ItemListItem.jsx
@@ -11,20 +11,22 @@ export default function ItemListItem({ storeItem, setShowItemDetail, setItemDeta
         setItemDetail(storeItem)
         // console.log(`I've been clicked: ${storeItem._id}`)
     }
-    const  handleAddClick = () => {
+    const handleAddClick = () => {
         addItemToCartClick(storeItem._id)
     }
 
     return (
-        <div className="item-card" onClick={handleDivClick}>
-            <div className="item-title-div">
-                <h3 className="item-title">{storeItem.title}</h3>
+        <div className="item-card">
+            <div className="item-background-click" onClick={handleDivClick}>
             </div>
-            <img className="item-img" src={storeItem.image} alt="itempic" />
-            <div className="item-card-bottom">
-                <p className="item-price">${storeItem.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-                <button className="item-card-btn-add" onClick={handleAddClick}>+ <span>Cart</span></button>
-            </div>
+                <div className="item-title-div">
+                    <h3 className="item-title">{storeItem.title}</h3>
+                </div>
+                <img className="item-img" src={storeItem.image} alt="itempic" />
+                <div className="item-card-bottom">
+                    <p className="item-price">${storeItem.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                    <button className="item-card-btn-add" onClick={handleAddClick}>+ <span>Cart</span></button>
+                </div>
         </div>
     )
 }
