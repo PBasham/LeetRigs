4942768d1d6c99ccbf00bd2f5ef0df6200c29e02
patrickbasham <PatrickTBasham@gmail.com>
2022-07-11 07:39:20 -0400

    updated styling for itemcards

--- src/components/ItemListItem/ItemListItem.jsx
+++ src/components/ItemListItem/ItemListItem.jsx
@@ -19,14 +19,14 @@ export default function ItemListItem({ storeItem, setShowItemDetail, setItemDeta
         <div className="item-card">
             <div className="item-background-click" onClick={handleDivClick}>
             </div>
-                <div className="item-title-div">
-                    <h3 className="item-title">{storeItem.title}</h3>
-                </div>
-                <img className="item-img" src={storeItem.image} alt="itempic" />
-                <div className="item-card-bottom">
-                    <p className="item-price">${storeItem.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-                    <button className="item-card-btn-add" onClick={handleAddClick}>+ <span>Cart</span></button>
-                </div>
+            <div className="item-title-div">
+                <h3 className="item-title">{storeItem.title}</h3>
+            </div>
+            <img className="item-img" src={storeItem.image} alt="itempic" />
+            <div className="item-card-bottom">
+                <p className="item-price">${storeItem.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                <button className="item-card-btn-add" onClick={handleAddClick}>+ <span>Cart</span></button>
+            </div>
         </div>
     )
 }
